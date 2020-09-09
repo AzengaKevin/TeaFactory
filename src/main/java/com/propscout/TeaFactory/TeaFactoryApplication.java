64a69d67bae8f3618f85e3a00bb690f0d1fb8eb3
Azenga Kevin <azenga.kevin7@gmail.com>
2020-09-09 19:35:39 +0300

    Initial commit, Added a vue engine and core web dependencies

---
+++ src/main/java/com/propscout/TeaFactory/TeaFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.propscout.TeaFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaFactoryApplication.class, args);
+	}
+
+}
