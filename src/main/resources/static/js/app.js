cd74207bf93b25884717ba140eb825adb76315bd
Azenga Kevin <azenga.kevin7@gmail.com>
2020-09-13 20:47:57 +0300

    WIP, Only display auth links in the user is not logged in, else Username and Dashbord Links

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,6 +5,12 @@ window.addEventListener('DOMContentLoaded', event => {
     //Get A Reference To The Sidebar
     const sidebar = document.getElementById('sidebar')
 
+    //Options Menu
+    const optionsMenu = document.getElementById('options-menu')
+
+    //Dropdown Menu
+    const dropdownMenu = document.getElementById('dropdownMenu')
+
     //Toggle the sidebar
     if (sidebarToggler) {
         sidebarToggler.addEventListener('click', sidebarTogglerEvent => {
@@ -12,6 +18,19 @@ window.addEventListener('DOMContentLoaded', event => {
             if (sidebar) {
                 if (sidebar.classList.contains('hidden')) sidebar.classList.remove('hidden')
                 else sidebar.classList.add('hidden')
+            }
+
+        })
+    }
+
+    //Toggle Dropdown Menu
+    if (optionsMenu) {
+
+        optionsMenu.addEventListener('click', optionsMenuEvent => {
+
+            if (dropdownMenu) {
+                if (dropdownMenu.classList.contains('hidden')) dropdownMenu.classList.remove('hidden')
+                else dropdownMenu.classList.add('hidden')
             }
 
         })
