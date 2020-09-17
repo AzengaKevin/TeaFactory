456b7ded11e25657a7814868eb7240166ce2666a
Azenga Kevin <azenga.kevin7@gmail.com>
2020-09-17 12:12:46 +0300

    Show cumulative monthly tea weight

--- src/main/java/com/propscout/teafactory/services/TeaRecordsService.java
+++ src/main/java/com/propscout/teafactory/services/TeaRecordsService.java
@@ -1,8 +1,14 @@
 package com.propscout.teafactory.services;
 
+import com.propscout.teafactory.models.MonthTeaWeight;
 import com.propscout.teafactory.models.entities.TeaRecord;
 import com.propscout.teafactory.repositories.TeaRecordRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TeaRecordsService {
@@ -19,6 +25,11 @@ public class TeaRecordsService {
         teaRecordRepository.save(teaRecord);
 
         return true;
+    }
+
+    public List<MonthTeaWeight> getThisMonthCumulativeTeaWeight() {
+
+        return teaRecordRepository.getCumulativeAccountTeaRecords();
     }
 
 
